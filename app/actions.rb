811c883f08b962eb5a197efbe27938110f83c7ba
Michelle Alarie <alarie.michelle@gmail.com>
2016-05-15 18:00:32 -0400

    Second Commit

--- app/actions.rb
+++ app/actions.rb
@@ -17,3 +17,41 @@ get '/messages/new' do
 end
 
 
+#below is from question 7: we want to add an action that allows us to respond to a url like /message/2 instead of defining an action for each potential message ID. By doing the below, we are defining one action that will accept ANY id in the url. After doing this is when we implemented the show.erb file; the :id part tells sinatra to amtch anything and give it pack to us as a "param". Since the :id is the record ID of the msg, we can use active record's 'find' method to grab it from the DB for us! 
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end 
+
+post '/messages' do
+  @message= Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+
+
+#we need to make sure that we aren'ts saving a message if all fields are left blank. So we need to validate it with Acitve Record. We will put the validation in the models/messages/message.rb file  
+#Question 9: No extra code was added below, but we added in code for the new.rb file to say that if @message had any errors to redirect them to a page that says that there was something wrong with their message entry attempt
+  if @message.save
+  redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
 
+#below we need to validate that the user doesn't leave the fields blank. In the actions.rb file, it is checking to see if the messages (once saved with @message.save) have the parameters that were outlined in the message.new 
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :content, presence: true, length: { maximum: 40}
+  validates :author, presence: true, length: { maximum: 25}
+
 end
